2201930efaca643ffb2298334cce3ae495834ee3
yasinalperbingul <55395610+yasinalperbingul@users.noreply.github.com>
2022-04-24 22:52:31 +0300

    fixed commit

--- circle.js
+++ circle.js
@@ -4,24 +4,27 @@ const context = canvas.getContext('2d');
 var circle = {
     x: 100,
     y: 100,
-    radius : 30
+    radius :10
 }
 
-var delayInMilliseconds = 1000;
+var delayInMilliseconds = 300;
 function start() {
     var i=0;
     interval = setInterval(function() {
         myArray = stateCircle(i);
         circle.x = myArray[0];
         circle.y = myArray[1];
+        context.beginPath();
         context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
         context.moveTo(0,0);
-        context.clearRect(0,0,800, 800);
+        context.closePath();
+        context.clearRect(0,0,600, 600);
         context.fill();
         i++;
         if(i==4){
             i=0;
         }
+        console.log(delayInMilliseconds);
     }, delayInMilliseconds);
 }
 
@@ -30,22 +33,22 @@ function stateCircle(state){
 
     switch(state){
         case 0:
-            position[0] = 200;
-            position[1] = 200;
+            position[0] = canvas.width/4;
+            position[1] = canvas.height/4;
             break;
         case 1:
-            position[0] = 600;
-            position[1] = 200;
+            position[0] = (canvas.width/4)*3;
+            position[1] = canvas.height/4;
             break;
         
         case 2:
-            position[0] = 200;
-            position[1] = 600;
+            position[0] = canvas.width/4;
+            position[1] = (canvas.height/4)*3;
             break;
 
         case 3:
-            position[0] = 600;
-            position[1] = 600;
+            position[0] = (canvas.width/4)*3;
+            position[1] = (canvas.height/4)*3;
             break;
         
         default:
@@ -60,9 +63,13 @@ function stop() {
 }
 
 function speedUp(){
-    delayInMilliseconds = delayInMilliseconds - 100;
+    delayInMilliseconds = delayInMilliseconds - 50;
+    clearInterval(interval);
+    start();
 }
 
 function speedDown(){
-    delayInMilliseconds = delayInMilliseconds + 100;
+    delayInMilliseconds = delayInMilliseconds + 50;
+    clearInterval(interval);
+    start();
 }
