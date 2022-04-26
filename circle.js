ff02161eed6948ea7f1522ea3746319fad348804
yasinalperbingul <55395610+yasinalperbingul@users.noreply.github.com>
2022-04-27 00:16:14 +0300

    grid system updated

--- circle.js
+++ circle.js
@@ -4,28 +4,33 @@ const context = canvas.getContext('2d');
 var circle = {
     x: canvas.width/6,
     y: canvas.height/6,
-    radius :10
+    radius :10,
+    press: 0
 }
 
 var delayInMilliseconds = 300;
 function start() {
-    var i=0;
-    interval = setInterval(function() {
-        myArray = stateCircle(i);
-        circle.x = myArray[0];
-        circle.y = myArray[1];
-        context.beginPath();
-        context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
-        context.moveTo(0,0);
-        context.closePath();
-        context.clearRect(0,0,600, 600);
-        context.fill();
-        i++;
-        if(i==4){
-            i=0;
-        }
-        console.log(delayInMilliseconds);
-    }, delayInMilliseconds);
+    circle.press += 1;
+    if(circle.press == 1){
+        var i=0;
+        interval = setInterval(function() {
+            myArray = stateCircle(i);
+            circle.x = myArray[0];
+            circle.y = myArray[1];
+            context.beginPath();
+            context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
+            context.moveTo(0,0);
+            context.closePath();
+            context.clearRect(0,0,600, 600);
+            context.fill();
+            i++;
+            if(i==4){
+                i=0;
+            }
+            console.log(delayInMilliseconds);
+        }, delayInMilliseconds);
+    }
+   
 }
 
 function stateCircle(state){
@@ -60,6 +65,7 @@ function stateCircle(state){
 
 function stop() {
     clearInterval(interval);
+    circle.press = 0;
 }
 
 function speedUp(){
